4967d0131b6e7b543403d4068ad6cdf1f7483fa6
Shad-islam <m.shadislam01@gmail.com>
2024-03-05 20:49:50 +0600

    Add first part

---
+++ script.js
@@ -0,0 +1,33 @@
+let nameInput = document.getElementById("nameInput");
+let submitBtn = document.getElementById("submit-btn");
+let emailInput = document.getElementById("emailInput");
+let list = document.getElementById("list");
+
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const information = {
+    name: nameInput.value,
+    email: emailInput.value,
+  };
+
+  console.log("Name:", information.name);
+  console.log("Email:", information.email);
+
+  addContact(information);
+});
+
+function addContact(info) {
+  let li = document.createElement("li");
+
+  li.innerHTML = `<div class="info">
+   <h3 id="nameDisplay">${info.name}</h3>
+   <h3 id="emailDisplay">${info.email}</h3>
+ </div>
+ <div class="btns">
+   <button>Edit</button>
+   <button>Delete</button>
+ </div>`;
+
+  list.appendChild(li);
+}
